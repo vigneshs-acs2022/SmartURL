cb0f0cca6c440429d7ab666b49eefbdf63a6a7b7
vigneshs-acs2022 <111042189+vigneshs-acs2022@users.noreply.github.com>
2024-06-12 17:37:39 +0530

    Update shorten_url.py

--- src/shorten_url.py
+++ src/shorten_url.py
@@ -31,14 +31,8 @@ def shorten_url(context,original_url, custom_alias=None, expiration_time=None, u
 
 
 async def main(context=None):
-    data = context.req
-    context.log(data)
-    payload = {
-    "originalURL": "https://agilecyber.com",
-    "user_id": "66694ad10002a9d51628",
-    "custom_alias": None,
-    "expiration_time":"2024-06-12 01:01:10"
-}
+    payload = context.req
+
     originalURL = payload.get('originalURL')
     user_id = payload.get('user_id')
     custom_alias = payload.get('custom_alias')
