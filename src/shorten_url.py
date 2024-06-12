e2d527248ac82f93088a6b8e2b67d0a6ef77f18a
vigneshs-acs2022 <111042189+vigneshs-acs2022@users.noreply.github.com>
2024-06-12 14:35:23 +0530

    Update shorten_url.py

--- src/shorten_url.py
+++ src/shorten_url.py
@@ -11,7 +11,7 @@ client.set_endpoint('https://cloud.appwrite.io/v1') \
       .set_project('666927680026e4e78388') \
       .set_key('f00160675e58ed23e0332fecf4a1ff21b84e73fb959c92cd155b2d1607c3333420cc3f742e73732448fa97ff78322b254bd880a8e667293f6ae0bd408fca1d0a91584b065e59b80bedf6daa5ffe5d410378433f7c46b4cffb90cda8a95b0b75f1018eb72f83cf7f8b6d6fbc573c3681e3672c4da76c8c5ca6ac8dd4f8291be5a')
 
-database = Database(client)
+database = Databases(client)
 
 async def shorten_url(original_url, custom_alias=None, expiration_time=None, user_id=None):
     url_id = custom_alias if custom_alias else hashlib.md5(original_url.encode()).hexdigest()[:6]
